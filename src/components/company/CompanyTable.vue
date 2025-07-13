a2aa5406b9da1840c024ee115eaa08680c7e4e1b
Dor <dorhakim100@gmail.com>
2025-07-13 15:41:54 +0300

    Add better icons to boolean fields

--- src/components/company/CompanyTable.vue
+++ src/components/company/CompanyTable.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="table-container">
-    <q-table title="Companies" :rows="companies" :columns="columns" row-key="id" flat bordered />
+    <q-table title="Companies" :rows="companies" :columns="columns" row-key="id" flat bordered>
+      <template #body-cell-active="props">
+        <q-td :props="props" class="icon-container">
+          <q-icon
+            :name="props.row.active ? 'check_circle' : 'cancel'"
+            :color="props.row.active ? 'positive' : 'negative'"
+          />
+        </q-td>
+      </template>
+
+      <template #body-cell-providesAiServices="props">
+        <q-td :props="props" class="icon-container">
+          <q-icon
+            :name="props.row.providesAiServices ? 'smart_toy' : 'remove_circle_outline'"
+            :color="props.row.providesAiServices ? 'primary' : 'grey'"
+          />
+        </q-td>
+      </template>
+
+      <template #body-cell-isDpfFound="props">
+        <q-td :props="props" class="icon-container">
+          <q-icon
+            :name="props.row.isDpfFound ? 'find_in_page' : 'highlight_off'"
+            :color="props.row.isDpfFound ? 'accent' : 'grey-6'"
+          />
+        </q-td>
+      </template>
+    </q-table>
   </div>
 </template>
 
@@ -14,5 +41,8 @@ const props = defineProps(['companies'])
 
 <style scoped lang="scss">
 .table-container {
+  .icon-container {
+    text-align: center;
+  }
 }
 </style>
