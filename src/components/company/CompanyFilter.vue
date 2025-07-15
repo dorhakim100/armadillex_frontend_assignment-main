43ea572991e0b602194d7155e2b058b9d6328918
Dor <dorhakim100@gmail.com>
2025-07-15 09:12:50 +0300

    Add better filter layout

--- src/components/company/CompanyFilter.vue
+++ src/components/company/CompanyFilter.vue
@@ -1,36 +1,43 @@
 <template>
-  <div class="container">
-    <q-input
-      filled
-      bottom-slots
-      :model-value="props.filter.txt"
-      label="Name"
-      @update:model-value="(val) => updateField('txt', val)"
-    >
-      <template v-slot:prepend>
-        <q-icon name="search" />
-      </template>
-      <template v-slot:append>
-        <q-icon name="close" class="cursor-pointer" @click="() => updateField('txt', '')" />
-      </template>
-    </q-input>
-    <q-select
-      filled
-      :model-value="props.filter.country"
-      :options="countries"
-      label="Filled"
-      @update:model-value="(val) => updateField('country', val)"
-      popup-content-class="custom-select-dropdown"
-    />
-    <!-- <date-picker  /> -->
+  <div class="filter-container q-pa-md">
+    <div class="filter-inputs">
+      <q-input
+        filled
+        dense
+        bottom-slots
+        :model-value="props.filter.txt"
+        label="Search Name"
+        @update:model-value="(val) => updateField('txt', val)"
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+        <template v-slot:append>
+          <q-icon name="close" class="cursor-pointer" @click="() => updateField('txt', '')" />
+        </template>
+      </q-input>
 
-    <q-checkbox
-      v-for="(checkbox, index) in checkboxFilters"
-      :key="index"
-      :model-value="props.filter[checkbox.key]"
-      :label="checkbox.label"
-      @update:model-value="(val) => updateField(checkbox.key, val)"
-    />
+      <q-select
+        filled
+        dense
+        :model-value="props.filter.country"
+        :options="countries"
+        label="Country"
+        @update:model-value="(val) => updateField('country', val)"
+        popup-content-class="custom-select-dropdown"
+      />
+    </div>
+
+    <div class="filter-checkboxes">
+      <q-checkbox
+        v-for="(checkbox, index) in checkboxFilters"
+        :key="index"
+        :model-value="props.filter[checkbox.key]"
+        :label="checkbox.label"
+        @update:model-value="(val) => updateField(checkbox.key, val)"
+        dense
+      />
+    </div>
   </div>
 </template>
 
@@ -53,21 +60,41 @@ function updateField(field, value) {
 </script>
 
 <style scoped lang="scss">
-.container {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
-
-  gap: 0.8rem;
-  // padding: 1.2rem;
+.filter-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  background-color: #f5f7fa;
   border-radius: 8px;
-  background-color: #fafafa;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
-}
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
+  gap: 1rem;
+  padding: 1rem 1.2rem;
 
-button {
-  margin-top: 1rem;
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  cursor: pointer;
+  @media (max-width: 600px) {
+    display: grid;
+  }
+
+  .filter-inputs {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+    gap: 1.2rem;
+    label {
+      padding-bottom: 0.3rem;
+    }
+
+    @media (max-width: 600px) {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  .filter-checkboxes {
+    display: flex;
+    flex: 1 1 300px;
+    flex-wrap: wrap;
+    align-items: center;
+
+    gap: 1.2rem;
+  }
 }
 </style>
