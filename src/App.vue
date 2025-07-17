0d5a7b1f217d17397445b408800378a57623e4e5
Dor <dorhakim100@gmail.com>
2025-07-17 18:34:53 +0300

    Add body dark-mode background

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,28 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, computed, watch } from 'vue'
 import { useSystemStore } from 'src/stores/system'
 import { notifyMsgs, notifyService } from './services/notify.service'
 
 const systemStore = useSystemStore()
+const isDarkMode = computed(() => systemStore.isDarkMode)
 
 onMounted(() => {
   notifyService.success(notifyMsgs.loginSuccess)
   systemStore.initializeScreenWatcher()
+  if (isDarkMode.value) {
+    document.querySelector('body').classList.add('dark-mode')
+  } else {
+    document.querySelector('body').classList.remove('dark-mode')
+  }
+})
+
+watch(isDarkMode, (newValue) => {
+  if (newValue) {
+    document.querySelector('body').classList.add('dark-mode')
+  } else {
+    document.querySelector('body').classList.remove('dark-mode')
+  }
 })
 </script>
